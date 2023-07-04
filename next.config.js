906c8150be9cc89ca55243062c584bae77fd866e
Mufaddal Aurangabadi <mufaddalaurangabadi@Mufaddals-MacBook-Pro.local>
2023-07-04 12:26:52 +0530

    final push

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+
+  // output: "export",
+
   experimental: {
     appDir: true,
     serverComponentsExternalPackages: ["mongoose"],
