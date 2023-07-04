0ed2a4f1b5a0154564acfa19791dd38376867aa5
Mufaddal Aurangabadi <mufaddalaurangabadi@Mufaddals-MacBook-Pro.local>
2023-07-04 14:00:03 +0530

    main

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-  output: "export",
+  // output: "export",
 
   experimental: {
     appDir: true,
@@ -23,7 +23,7 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  
+
   typescript: {
     ignoreBuildErrors: true,
   },
