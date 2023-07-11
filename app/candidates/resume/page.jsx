ddb8fc35ba0574e49f74dc74e55734354d5195e1
Mufaddal Aurangabadi <mufaddalaurangabadi@Mufaddals-MacBook-Pro.local>
2023-07-11 17:23:17 +0530

    api fix

--- app/candidates/resume/page.jsx
+++ app/candidates/resume/page.jsx
@@ -104,7 +104,7 @@ const Resume = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("https://medirecruiters.in/api/candidate", {
+      const response = await fetch("https://medirecruiters.in/api/candidates", {
         method: "POST",
         body: JSON.stringify({
           firstName: firstName,
